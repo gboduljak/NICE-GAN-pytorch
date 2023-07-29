0c00d0d8da0f2c2a10205410a1c6b6b60a1b271c
Gabrijel Boduljak <gboduljak@outlook.com>
2023-07-29 02:57:45 +0200

    fixed loading

--- NICE_CUT.py
+++ NICE_CUT.py
@@ -605,7 +605,7 @@ class NICE_CUT(object):
         device=self.device
     )
     _, _,  _, _, real_A_z = self.disB(x)
-    self.patch_sampler(self.generator(real_A_z, nce=True))
+    self.patch_sampler(self.gen2B(real_A_z, nce=True))
     self.patch_sampler.load_state_dict(params['patch_sampler'])
     self.start_iter = params['start_iter']
 
