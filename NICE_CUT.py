c74931ba8978e696f733e20d3ca565fb75a6182a
Gabrijel Boduljak <gboduljak@outlook.com>
2023-07-29 03:04:48 +0200

    minor

--- NICE_CUT.py
+++ NICE_CUT.py
@@ -308,7 +308,6 @@ class NICE_CUT(object):
 
     for step in range(self.start_iter, self.iteration + 1):
       self.step = step
-      print(step)
 
       if self.decay_flag and step > (self.iteration // 2):
         self.G_optim.param_groups[0]['lr'] -= (self.lr / (self.iteration // 2))
